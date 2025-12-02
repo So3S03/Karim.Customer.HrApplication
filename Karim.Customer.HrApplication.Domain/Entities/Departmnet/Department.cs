1e97fbbc21c0801a709575b4cbd3b8698a1e0cfc
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2025-12-03 00:14:05 +0200

    added more fields for the department database entity

--- Karim.Customer.HrApplication.Domain/Entities/Departmnet/Department.cs
+++ Karim.Customer.HrApplication.Domain/Entities/Departmnet/Department.cs
@@ -11,7 +11,11 @@ namespace Karim.Customer.HrApplication.Domain.Entities.Departmnet
         public bool isActive { get; set; }
         public DateTime ActualCreationDate { get; set; }
         public string? DepartmentPhotoUrl { get; set; }
-        public decimal DepartmentBudget { get; set; }
+        public decimal TotalDepartmentBudget { get; set; }
+        public decimal DepartmentBudgetForSalaries { get; set; }
+        public decimal DepartmentBudgetForTools { get; set; }
+        public decimal DepartmentBudgetForTrainees { get; set; }
+        public decimal? DepartmentBudgetOther { get; set; }
         //public int NumberOfProjects { get; set; } //it will came from project module
         //public int DepartmentEmployeesNumber { get; set; } //it will came from employee module
     }
