b64bb84ad910acd6ee3229124fd14084a166f050
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2025-12-02 23:32:11 +0200

    Created GenaricRepository & UnitOfWork Design Patterns

---
+++ Karim.Customer.HrApplication.Infrastructure.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Karim.Customer.HrApplication.Domain.Entities.BaseEntities;
+using Karim.Customer.HrApplication.Domain.GenaricRepository;
+using Karim.Customer.HrApplication.Domain.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Concurrent;
+
+namespace Karim.Customer.HrApplication.Infrastructure.Persistence.UnitOfWork
+{
+    public class UnitOfWork(DbContext dbContext) : IUnitOfWork
+    {
+        private readonly ConcurrentDictionary<string, object> storedRepos = new ConcurrentDictionary<string, object>();
+        public IGenaricRepository<TEntity, TKey> GenerateRepository<TEntity, TKey>()
+            where TEntity : BaseEntity<TKey>
+            where TKey : IEquatable<TKey>
+            => (IGenaricRepository<TEntity, TKey>) storedRepos.GetOrAdd(typeof(TEntity).Name, new GenaricRepository.GenaricRepository<TEntity, TKey>(dbContext));
+
+        public async Task<int> CompleteAsync() => await dbContext.SaveChangesAsync();
+
+        public async ValueTask DisposeAsync() => await dbContext.DisposeAsync();
+    }
+}
