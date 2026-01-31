4ebc553654582be5b108db04cfbfde998d83fe22
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2026-01-31 23:12:00 +0200

    Prepared Employee Module With Adding Relations Between Employee Module & Department Module

---
+++ Karim.Customer.HrApplication.Domain/Entities/Employee/EmployeeType.cs
@@ -0,0 +1,9 @@
+﻿namespace Karim.Customer.HrApplication.Domain.Entities.Employee
+{
+    public enum EmployeeType
+    {
+        Contract = 1,
+        Freelance = 2,
+        LongLife = 3
+    }
+}
---
+++ Karim.Customer.HrApplication.Infrastructure.Persistence/Migrations/HRMSMigrations/20260131211008_CreatedEmployeeModuleWithAddingRelationsWithDepartment.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Karim.Customer.HrApplication.Infrastructure.Persistence.Migrations.HRMSMigrations
+{
+    /// <inheritdoc />
+    public partial class CreatedEmployeeModuleWithAddingRelationsWithDepartment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ManagerId",
+                table: "Department",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    EmployeeCode = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonalEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExtraPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WorkLocation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsHasContract = table.Column<bool>(type: "bit", nullable: false),
+                    ContractEndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(22,2)", precision: 22, scale: 2, nullable: true),
+                    JoinDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EmployeeStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Department_ManagerId",
+                table: "Department",
+                column: "ManagerId",
+                unique: true,
+                filter: "[ManagerId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DepartmentId",
+                table: "Employee",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_EmployeeCode",
+                table: "Employee",
+                column: "EmployeeCode",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Employee_ManagerId",
+                table: "Department",
+                column: "ManagerId",
+                principalTable: "Employee",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Employee_ManagerId",
+                table: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Department_ManagerId",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "ManagerId",
+                table: "Department");
+        }
+    }
+}
