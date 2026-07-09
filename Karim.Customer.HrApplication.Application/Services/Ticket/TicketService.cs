8fe2ef208c28cf197d1732ccdaf8a752805204a7
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2026-07-10 01:33:14 +0300

    Modified Close Ticket Busseniss Logic

--- Karim.Customer.HrApplication.Application/Services/Ticket/TicketService.cs
+++ Karim.Customer.HrApplication.Application/Services/Ticket/TicketService.cs
@@ -290,6 +290,26 @@ namespace Karim.Customer.HrApplication.Application.Services.Ticket
             if (Ticket is null) throw new NotFoundException("Ticket Not Exist!");
             //Check If Ticket Already Closed
             if (Ticket.Status == TicketStatus.Closed) throw new ConflictException("Ticket Already Closed");
+            //Check On Project
+            if (Ticket.Project is null) throw new BadRequestException("Can't Operate On Ticket That Has No Project!");
+            //Check On Project Status
+            switch (Ticket.Project.ProjectStatus)
+            {
+                case Domain.Entities.Projects.ProjectStatus.Draft:
+                    throw new ConflictException("Can't Close Ticket That Have Draft Project");
+                case Domain.Entities.Projects.ProjectStatus.Cancelled:
+                    throw new ConflictException("Can't Close Ticket That Have Cancelled Project!");
+                case Domain.Entities.Projects.ProjectStatus.OnHold:
+                    throw new ConflictException("Can't Close Ticket That Have OnHold Project!");
+            }
+            //Create Tasks Repo
+            var TasksRepo = _unitOfWork.GenerateRepository<Domain.Entities.Tasks.Tasks, string>();
+            //Create Spec
+            var TasksSpec = new InProgressTasksByTicketId(Ticket.Id);
+            //Get All Tasks
+            var TasksList = await TasksRepo.GetAllAsync(TasksSpec);
+            //Check If Ticket Has Tasks On Going && Status == InProgress
+            if (TasksList.Any()) throw new ConflictException("Can't Close Ticket That Has OnGoing Tasks");
             //Change Status
             Ticket.Status = TicketStatus.Closed;
             //Update
