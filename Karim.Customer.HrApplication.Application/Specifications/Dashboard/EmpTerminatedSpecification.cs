c3eb89f6b131d14a893a265b8b169bc18e442cf1
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2026-07-29 16:27:51 +0300

    Created Api For Retrive Count Of Hired Vs Terminated Per Month On Specific Year

---
+++ Karim.Customer.HrApplication.Application/Specifications/Dashboard/EmpTerminatedSpecification.cs
@@ -0,0 +1,8 @@
+﻿using employee = Karim.Customer.HrApplication.Domain.Entities.Employee.Employee;
+
+namespace Karim.Customer.HrApplication.Application.Specifications.Dashboard
+{
+    internal class EmpTerminatedSpecification(int year) : BaseSpecifications<employee, string>(E => E.TerminateResignedDate.Value.Year == year)
+    {
+    }
+}
