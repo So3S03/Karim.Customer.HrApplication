1b5b099d4e7f358882d5c85b310c243789ff6e37
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2026-07-29 23:00:14 +0300

    Created New Api For Get Count Of Employee Per Departments

--- Karim.Customer.HrApplication.Application.Abstraction/ServicesContract/Dashboard/IDashboardService.cs
+++ Karim.Customer.HrApplication.Application.Abstraction/ServicesContract/Dashboard/IDashboardService.cs
@@ -8,5 +8,6 @@ namespace Karim.Customer.HrApplication.Application.Abstraction.ServicesContract.
         Task<ICollection<PayrollComparisonPerMonthDto>> GetMonthlyPayrollsSumComparison(int? year);
         Task<ICollection<AllEmployeesAttendanceRatePerMonthDto>> GetAttendanceRatePerMonthComparison(int? year);
         Task<ICollection<HiringVsResignedOrTerminatedEmployeesDto>> GetHiringVsResignedOrTermiunatedPerMonthComparison(int? year);
+        Task<ICollection<CountOfEmployeeInDepartmentsDto>> GetCountOfEmployeesInDepartments();
     }
 }
