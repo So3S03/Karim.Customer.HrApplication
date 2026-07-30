faff04d68aea32b3b7a306f52316c644ddecb212
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2026-07-30 22:15:35 +0300

    Fixed Attendance Summary Per Employee API

--- Karim.Customer.HrApplication.Shared/DTOs/Attendance/FingerprintSummaryDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Karim.Customer.HrApplication.Shared.DTOs.Attendance
-{
-    public class FingerprintSummaryDto
-    {
-        public int LateForWorkCount { get; set; }
-        public int DelayInDurationCount { get; set; }
-    }
-}
