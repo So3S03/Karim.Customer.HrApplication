b9417b3ea76564e9d55318f4218d8fb3075ecf91
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2026-08-03 16:39:16 +0300

    Modified Unauthorized Exception

--- Karim.Customer.HrApplication.Shared/Exceptions/UnauthorizedException.cs
+++ Karim.Customer.HrApplication.Shared/Exceptions/UnauthorizedException.cs
@@ -1,6 +1,15 @@
 ﻿namespace Karim.Customer.HrApplication.Shared.Exceptions
 {
-    public class UnauthorizedException(): Exception("You Are Not Authenticated To See This Application")
+    public class UnauthorizedException: Exception
     {
+        public UnauthorizedException() : base("You Are Not Authenticated To See This Application")
+        {
+            
+        }
+
+        public UnauthorizedException(string message) : base(message)
+        {
+            
+        }
     }
 }
